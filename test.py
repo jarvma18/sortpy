30584a50579bcddf9a79f63be6115159469beddb
jarvma18 <markusmjarvinen@outlook.com>
2023-12-29 01:50:30 +0200

    Added unit test to test sorting lexiographically and reading a file.

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+from main import sortLexiographically
+from main import readFile
+
+class TestClass(unittest.TestCase):
+  def test_readFile(self):
+    testFile = 'short-text.txt'
+    expectedValue = 'Short text'
+    self.assertEqual(expectedValue, readFile(testFile))
+  def test_sortLexiographically(self):
+    testData = 'a\nb\nc'
+    expectedValue = ['a', 'b', 'c']
+    self.assertEqual(expectedValue, sortLexiographically(testData))
