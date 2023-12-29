13f79c915bb1e7d39e85adddd08c3d635e34dfad
jarvma18 <markusmjarvinen@outlook.com>
2023-12-29 13:31:02 +0200

    Separated file opening to own function so that it is short in the future when I added the expection if file is not found, added some unittests for open not existing file and for printing the array of words.

--- main.py
+++ main.py
@@ -10,16 +10,29 @@ def sortLexiographically(contentToSort):
   sortedContent.sort()
   return sortedContent
 
+def openFile(fileName):
+  try:
+    file = open(fileName, 'r')
+  except FileNotFoundError:
+    raise Exception('File not found, check that the file exists in that path')
+  else:
+    return file
+
 def readFile(fileName):
-  file = open(fileName, 'r')
-  fileContent = file.read()
-  file.close()
-  return fileContent
+  file = openFile(fileName)
+  if file:
+    fileContent = file.read()
+    file.close()
+    return fileContent
+  else:
+    return
 
 def readFileAndSortContent(fileName):
   rawFileContent = readFile(fileName)
   sortedFileContent = sortLexiographically(rawFileContent)
   return sortedFileContent;
+
+# def readPassedArguments(arguments):
 
 sortedContent = readFileAndSortContent(sys.argv[1])
 printWordsFromArray(sortedContent)
--- test.py
+++ test.py
@@ -1,17 +1,39 @@
 import unittest
+import io
+import sys
 from main import sortLexiographically
 from main import readFile
 from main import readFileAndSortContent
+from main import openFile
+from main import printWordsFromArray
 
 class TestClass(unittest.TestCase):
+  # Prints can be tested with io and sys
+  def test_printWordsFromArray(self):
+    capturedOutput = io.StringIO()
+    sys.stdout = capturedOutput
+    testData = ['a', 'b', 'c', 'd', 'e']
+    expectedValue = 'a\nb\nc\nd\ne\n'
+    printWordsFromArray(testData)
+    sys.stdout = sys.__stdout__
+    self.assertEqual(expectedValue, capturedOutput.getvalue())
+
+  def test_openNotExistingFile(self):
+    expectionMessage = 'File not found, check that the file exists in that path'
+    testFile = 'test-materials/this-file-should-not-exist.txt'
+    with self.assertRaises(Exception) as context:
+      openFile(testFile)
+    self.assertTrue(expectionMessage in str(context.exception))
+
+  # Here we also test that the file is opened and can be read
   def test_readFile(self):
     testFile = 'test-materials/short-text.txt'
     expectedValue = 'Short text'
     self.assertEqual(expectedValue, readFile(testFile))
 
   def test_sortLexiographically(self):
-    testData = 'a\nb\nc'
-    expectedValue = ['a', 'b', 'c']
+    testData = 'a\nb\nc\nd\ne'
+    expectedValue = ['a', 'b', 'c', 'd', 'e']
     self.assertEqual(expectedValue, sortLexiographically(testData))
 
   def test_readFileAndSortContent(self):
